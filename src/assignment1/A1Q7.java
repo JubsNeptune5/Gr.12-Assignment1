134d2a889220d1dafee7c5291670df36af823e8c
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-09-08 10:13:18 -0400

    School Sept.8

---
+++ src/assignment1/A1Q7.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1;
+
+/**
+ *Determine when the population will exceed 10 billion
+ * @author laveh2107
+ */
+public class A1Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //counters
+        //600 because 6 billion is to large to store in a double
+        double pop = 600; 
+        //integer to keep track of the years between 6 bil. and 10 bil.
+        int years = 1999;
+        
+        while ( pop <= 1000){
+            
+        }
+    }
+}
