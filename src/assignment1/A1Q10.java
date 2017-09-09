e6e3735ae02a4135a6657fd08a36d220308f8144
hunterlaverty <hunterlaverty@192.168.100.231>
2017-09-09 17:34:17 -0400

    home Sept 9

--- src/assignment1/A1Q10.java
+++ src/assignment1/A1Q10.java
@@ -23,17 +23,25 @@ public class A1Q10 {
         //cretae string
         String word = new String();
 
+        //create array for the sentance
         String[] sentence = new String[1000];
 
+        //create counter for the number of letters 
         int count = 0;
         System.out.println(" Enter a sentence typing enter after each word");
 
+        //Go through each spot int he array
         for (int i = 0; i < sentence.length; i++) {
+            //Input a word
             sentence[i] = Input.nextLine();
+            //Change to lower case
             sentence[i] = sentence[i].toLowerCase();
+            //when the word reads exit, the program reads out the count of letters
             if (sentence[i].equals("exit")) {
                 System.out.println("You said " + count + " letters before you typed exit");
+            break;
             }
+            //Add the number of letter of the word that wasnt exit to the other count
             count = count + sentence[i].length();
         }
     }
